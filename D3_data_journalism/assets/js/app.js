cfe39c6b470c7c068c5d11a9513afcc6036fe831
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-07-19 17:51:07 -0500

    Created svg, added file location

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,5 +1,48 @@
 // @TODO: YOUR CODE HERE!
-d3.csv("./data/data.csv").then(function(censusData){
+//// Make the space for the graph in the html ( go to html first to see where everything goes)
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+  };
+
+  // chart area minus margins
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+
+// create svg container
+var svg = d3.select("#scatter").append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+  /// Create the svg (select, append, etc.)
+
+d3.csv("assets/data/data.csv").then(function(censusData){
+    console.log(censusData);
+
+// cast everything so they become integers 
+
+
+// get y scale (height) scaleLinear
+
+// get x scale (width) also scaleLinear
+
+// create axes
+
+//append data to chart 
+
+// select existing svg in the html 
+// select all, select data, enter data, append the circles and add attributes 
+
+
+
+
+
 
 // closing parenthesis for csv reading 
 });
