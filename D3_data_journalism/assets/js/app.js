db3035f982cbb672ad85e876fc7e74135180691d
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-07-19 20:37:29 -0500

    Finished circle graph ( no states)

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -23,37 +23,58 @@ var svg = d3.select("#scatter").append("svg")
 
   /// Chartgroup??? 
   var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
   /// Open CSV document 
 d3.csv("assets/data/data.csv").then(function(censusData){
     console.log(censusData);
 
 // Choose which graph Smokers (censusData.smokes) vs. Age (censusData.age) or Healthcare (censusData.healthcare) vs. Poverty (censusData.poverty)
-    // cast everything so they become integers 
-censusData.forEach(data =>{
+    // cast everything so they become integers except state abbreviation used for the circles 
+censusData.forEach(data => {
     data.smokes = +data.smokes;
     data.age = +data.age ;
     data.healthcare = +data.healthcare; 
     data.poverty = +data.poverty;
-    var states = data.abbr;
-    console.log(states);
+  //  var states = data.abbr;
+   // console.log(states);
 });
 
-
 // get y scale (height) scaleLinear
+var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(censusData, d => d.smokes)])
+      .range([chartHeight, 0])
 
 // get x scale (width) also scaleLinear
+var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(censusData, d => d.age)])
+      .range([0, chartWidth]);
 
-// create axes
+// create axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
 
-//append data to chart 
+  //append data to chart 
+  chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(bottomAxis);
 
-// select existing svg in the html 
+    chartGroup.append("g")
+      .call(leftAxis);
+
 // select all, select data, enter data, append the circles and add attributes 
+var circlesGroup = chartGroup.selectAll("circle")
+.data(censusData)
+.enter()
+.append("circle")
+.attr("cx", d => xLinearScale(d.age))
+.attr("cy", d => yLinearScale(d.smokes))
+.attr("r", "15")
+.attr("fill", "blue")
+.attr("opacity", ".5");
 
 
-
+// Create the tooltips 
 
 
 
